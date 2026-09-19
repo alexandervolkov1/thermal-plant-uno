53924bedcfae3fe3c29b939be6ef0632775df4ed
a.volkov <a.volkov111555@gmail.com>
2026-09-19 15:24:34 +0300

    feat: add UART echo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::prelude::*;
 use panic_halt as _;
 
 #[arduino_hal::entry]
@@ -8,10 +9,11 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    let mut led = pins.d13.into_output();
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
     loop {
-        led.toggle();
-        arduino_hal::delay_ms(1000);
+        let input = nb::block!(serial.read()).unwrap_infallible();
+
+        nb::block!(serial.write(input)).unwrap_infallible();
     }
 }
